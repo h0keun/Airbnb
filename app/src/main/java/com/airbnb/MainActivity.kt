bef399f7bee3c5230e6a69f81b67469939adb153
h0keun <dlghrms95@gmail.com>
2021-05-08 19:09:46 +0900

    permission, map_fun

--- app/src/main/java/com/airbnb/MainActivity.kt
+++ app/src/main/java/com/airbnb/MainActivity.kt
@@ -2,9 +2,20 @@ package com.airbnb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.naver.maps.map.MapView
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
 
-class MainActivity : AppCompatActivity() {
+//네이버 맵 api 를 이용하는 두가지 경우가 있는데
+//하나는 프래그먼트로 하나는 맵뷰로 둘의 차이는 프래그먼트는
+//생영주기를 신경쓰지 않아도 되지만 커스텀이 어렵
+//앱뷰는 여러 커스텀이 가능하지만 생명주기상 이슈가 있어서
+//아래처럼 직접 설정해주어야함함
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
 
     private val mapView: MapView by lazy {
         findViewById(R.id.mapView)
@@ -14,6 +25,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mapView.onCreate(savedInstanceState)
+
+        // MapReady 라는 함수의 인자로는 OnMapReadyCallback 이라는
+        // 인터페이스의 구현체가 담긴것이 들어가야하는데,
+        // 현재 OnMapReadyCallback 을 MainActivity 에서 implement 했고,
+        // OnMapReady 에 대한 구현을 메인엑티비티에 직접 해주었기 때문에
+        // 메인액티비티가 OnMapReadyCallback 의 구현체라고도 할 수 있다
+        // 때문에 아래에 this 가 가능
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+
+        naverMap.maxZoom = 15.0
+        naverMap.minZoom = 4.0
+
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.497885, 127.027512))
+        naverMap.moveCamera(cameraUpdate)
+
+        val uiSetting = naverMap.uiSettings
+        uiSetting.isLocationButtonEnabled = true
+
+        locationSource = FusedLocationSource(this@MainActivity, LOCATION_PERMISSION_REQUEST_CODE)
+        naverMap.locationSource = locationSource
+
+        val marker = Marker()
+        marker.position = LatLng(37.623577, 126.825321)
+        marker.map = naverMap // 다양한 다른 기능들은 네이버 공식 가이드라인에..
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return
+        }
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) {
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
     }
 
     override fun onStart() {
@@ -49,5 +102,9 @@ class MainActivity : AppCompatActivity() {
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
